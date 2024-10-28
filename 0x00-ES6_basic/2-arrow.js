102c3a2c642d1cadf5a3009a57ffe8c62b3e219f
Michael <lordkay07@gmail.com>
2024-10-28 10:41:24 +0100

    rewriting arrow function

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -2,9 +2,7 @@ export const getNeighborhoodsList = () => {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
   const self = this;
-  this.addNeighborhood =  function add(newNeighborhood) {
+  this.addNeighborhood = (newNeighborhood)=> {
     return self.sanFranciscoNeighborhoods.push(newNeighborhood);
   };
 };
-
-
